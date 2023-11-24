ce11452983d5a7fc79207798e081e0e72cad0f71
Ollie <Ollie@Ollie>
2023-11-24 01:30:13 +0000

    version 0.001

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# sigsearch
+# info
+pretty bad grep clone, only v0.0001 so dont expect it to work
+
+
+
+# use
+just drop your txt files into the sigsearch folder
+
+
+
+# credits
+
+youtube, ollie
+
+
+
+feel free to change it around 
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,30 @@
 use std::error::Error;
 use std::fs;
 
+// go away
+
 pub fn run(config: Cfg) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename).expect("someting wong");        // my code is very neat ik
-    println!("Text: \n {} ", contents);
+   for line in search(&config.query, &contents){
+
+    println!("{}", line);
+
+
+   }
     Ok(())
 } 
+
+
+
+
 pub struct Cfg { //config
     pub query: String,
     pub filename: String,
 
 
 }
+
+
 impl Cfg {
     pub fn new(args: &[String]) -> Result<Cfg, &str>{
         if args.len() < 3 {
@@ -24,6 +37,31 @@ impl Cfg {
     
     }
 
+
+
+}
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn oneresult(){
+        let query = "tes";
+        let content = "test, tes, te, p";
+        assert_eq!(vec!["test, tes, te, p"], search(query,content));
+
+
+    }
 
 
 }
